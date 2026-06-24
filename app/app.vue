ee8fc1305d948bd14d48b1f31e7dc60a52b57913
Clemens Sietas <siecle@gmx.de>
2026-06-24 14:17:40 +0200

    Landing page created

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="p-10">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
